4ed3484dbeef99253efd4b8b980a67225edbe455
POLA-melvin <poamelvin1@gmail.com>
2026-01-29 01:50:38 +0100

    Initial e-commerce app with About page

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+function AboutUs() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>About Our Store DEMONSTRATION PAGE</h1>
+      <p>
+        This e-commerce frontend demonstrates DevOps CI/CD automation using
+        Docker and Kubernetes.
+
+        this is purefly for demonstration purpose.
+      </p>
+    </div>
+  );
+}
+export default AboutUs;
